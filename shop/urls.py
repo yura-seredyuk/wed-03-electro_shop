a47a9e75027c888d0b2d3fb5095c3f9b4d848a0b
Юрій Середюк <yura2590@gmail.com>
2021-09-28 18:56:07 +0300

    Fist themplate settinfs

---
+++ shop/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    url(r'^$', views.homepage, name='home'),  
+    # # url(r'^post/(?P<pk>[0-9]+)$', views.post_datail, name='post_datail'),
+    # path('post/<int:pk>', views.post_detail, name='post_detail'),
+    # # url('post/(?P<pk>[0-9]+)', views.post_detail, name='post_detail'),
+    # url(r'^post/create/$', views.post_create, name='post_create'),
+    # url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    # url(r'^post/(?P<pk>[0-9]+)/delete/$', views.post_delete, name='post_delete'),
+    # url(r'^contact/$', views.contact, name='contact'),
+    # url(r'^login/$', views.user_login, name='login'),
+]
