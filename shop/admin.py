8ea773cacb6e9a41b7d7396b7e02e1f1e25e4f4e
Юрій Середюк <yura2590@gmail.com>
2021-09-30 19:29:32 +0300

    Add models

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Category, Brand, Product
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug':('name',)}
+admin.site.register(Category, CategoryAdmin)
+
+class CategoryBrand(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug':('name',)}
+admin.site.register(Brand, CategoryBrand)
+
+class CategoryProduct(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'old_price', 'rait', 'stock', 'available', 'created', 'updated']
+    list_filter = ['rait', 'available', 'created', 'updated']
+    list_editable = ['price', 'old_price', 'stock', 'available']
+    prepopulated_fields = {'slug':('name',)}
+admin.site.register(Product, CategoryProduct)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self) -> str:
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
+
+class Brand(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_brand', args=[self.slug])
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', null=True, on_delete=models.SET_NULL)
+    brand = models.ForeignKey(Brand, related_name='products', null=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    main_photo = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    photo_1 = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    photo_2 = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    photo_3 = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField()
+    details = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    old_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    rait = models.PositiveIntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
+
+
+
+    
+
